8675af045d6e3e07aecc162399ad65f4d6a45f51
Rob Watson <source.pot.dev@gmail.com>
2024-11-24 11:29:56 +0000

    Remove console.logs

--- src/lib/events/emitter.ts
+++ src/lib/events/emitter.ts
@@ -1,25 +1,22 @@
 import {Event, EventPayload} from './types'
 
-type Subscriber<Payload extends EventPayload> = (payload: Payload) => void
+type Subscriber<Payload extends EventPayload> = (payload: Payload) => void | Promise<void>
 
 const subscribers = new Map<string, Set<Subscriber<EventPayload>>>()
 
 export function emit<T extends Event<EventPayload>>(eventName: T['name'], payload: T['payload']) {
-  console.log('Emitting event', eventName)
   if (subscribers.has(eventName)) {
     subscribers.get(eventName)!.forEach(subscriber => subscriber(payload));
   }
 }
 
 export function subscribe<T extends Event<EventPayload>>(eventName: T['name'], subscriber: Subscriber<T['payload']>) {
-  console.log('Registering subscriber to event', eventName)
   if (!subscribers.has(eventName)) {
     subscribers.set(eventName, new Set())
   }
 
   subscribers.get(eventName)!.add(subscriber)
   return () => {
-    console.log('Unregistering subscriber to event', eventName)
     subscribers.get(eventName)!.delete(subscriber)
   }
 }
